80b7e35a5df450a12a34d1136dce8d8484807bad
sanjanenic <sanjanenicbl@gmail.com>
2022-03-14 14:16:03 +0100

    Array, Recursion, noch zu schauen, Matrix muss ich noch veerbessern

--- src/com/company/Matrix.java
+++ src/com/company/Matrix.java
@@ -2,9 +2,31 @@ package com.company;
 
 public class Matrix {
     public static void main(String[] args) {
+        generateIdentityMatrix(7);
 
     }
+    public static double[][] generateIdentityMatrix(int size){
+        double [][] tabelle = new double [4][3];
+        for(int zeile=0; zeile <tabelle.length; ++zeile){
+            for (int spalte=0; spalte<tabelle[zeile].length; ++spalte) {
+                if(zeile==spalte){
+                    System.out.print(tabelle[zeile][spalte] =1);
+                    System.out.println(" ");
+                   // System.out.println(java.util.Arrays.toString(tabelle [zeile][spalte]) );
+                }
+
+                else{
+                    tabelle[zeile][spalte] =0;
+                    System.out.print( tabelle[zeile][spalte] =0);
+                }
+            }
+
+        }
+        return tabelle;
+    }
 }
+
+
 
 //alle weten sollen auf die zahl 7 gesetzen werden!
 //int [][] tabelle = new int [4][3];
